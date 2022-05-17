17bc246a5c2c00c69ea3a2fc3da11a6e7be4c09c
travis1999 <trevoroguna@gmail.com>
2022-05-17 14:18:47 +0300

    base for authenication system

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Check if the request is authenticated or not
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Get the authorization header from the request
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """
+        Get the current user from the request
+        """
+        return None
